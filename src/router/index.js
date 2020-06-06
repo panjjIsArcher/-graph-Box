d4e37ea41309f85c97286d97e1c7f5568744a6d5
panjiajie <251907882@qq.com>
2020-06-06 15:44:00 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
-const routes = [];
+const routes = [
+  {
+    path:"/",
+    component:()=> import("../views/index/index")
+  },
+  {
+
+  }
+];
 const mode = "history";
 const router = new VueRouter({
     routes,
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+const resolve = dir => path.join(__dirname, dir);
 module.exports = {
   assetsDir: "", 
   lintOnSave:false,
@@ -23,5 +25,9 @@ module.exports = {
           } 
         ]
       }
+  },
+  chainWebpack: config => {
+    config.resolve.alias
+      .set("@", resolve("src"));
   }
 }
